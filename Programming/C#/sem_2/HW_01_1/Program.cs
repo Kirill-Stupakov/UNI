791d85d2cdf5cfc9d68621abe636d91bd67b830e
Kirill-Stupakov <kirill.stupakov.0@gmail.com>
2021-02-19 20:10:16 +0400

    fixed bugs

--- Programming/C#/sem_2/HW_01_1/Program.cs
+++ Programming/C#/sem_2/HW_01_1/Program.cs
@@ -6,13 +6,18 @@ namespace HW_01_1
     {
         static void Main(string[] args)
         {
-            // string[] str = Console.ReadLine().Split(' ');
-            // int[] array = new int[str.Length];
+            string[] str = Console.ReadLine().Split(' ');
+            int[] array = new int[str.Length];
             
-            // for (int i = 0; i < array.Length; i++)
-            //     array[i] = int.Parse(str[i]);
+            for (int i = 0; i < array.Length; i++)
+                array[i] = int.Parse(str[i]);
 
-            // SelectionSort(array);
+            Sort.SelectionSort(array);
+
+            foreach (int x in array)
+                Console.Write($"{x} ");
+            Console.WriteLine();
+
             if (Test.SortTest() == true)
                 Console.WriteLine("All tests passed!");
 
